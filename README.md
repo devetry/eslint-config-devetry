4aec78b70db95afdb0e85c10380ec34bc5fa145a
Brian Schiller <bgschiller@gmail.com>
2018-03-26 13:39:15 -0600

    defend choices

---
+++ README.md
@@ -0,0 +1,11 @@
+eslint-config-devetry
+=====================
+
+[Airbnb's eslint config](https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb-base) provides a good starting point, but we don't always agree with their choices.
+
+Rather than copy-pasting the same rules around different projects, we can now:
+
+1. `npm install --save-dev eslint-config-devetry`
+2. Add `extends: "devetry"` to your .eslintrc.
+
+We will try to add reasons for our deviations from airbnb's base, where appropriate.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: 'airbnb-base',
   rules: {
+    // "for (var i; i < 10; i++)" is well-established and unambiguous.
     'no-plusplus': ['error', {
       allowForLoopAfterthoughts: true,
     }],
@@ -14,12 +15,25 @@ module.exports = {
     'object-curly-newline': ['error', {
       consistent: true,
     }],
-    'arrow-body-style': 0,
+    // const
+    //  a = 1
+    //  b = a + 1;
+    // is a nice pattern.
     'one-var': 0,
     'no-cond-assign': [2, 'except-parens'],
     'quotes': [2, 'single', 'avoid-escape'],
+    // When writing a module that will have several exports,
+    // I don't want eslint telling me to default-export the first one,
+    // just because it doesn't know about the others yet.
+    // The author of the module should make the decision.
     'import/prefer-default-export': 'off',
+    // Sometimes this is just confusing.
+    // const target = { e };
+    // vs
+    // const target = e.target;
     'prefer-destructuring': 0,
+    // As in this object, there are some keys that require quoting, and
+    // some that don't. It's ugly IMO to have only some keys quoted.
     'quote-props': ['error', 'consistent'],
   },
 };
